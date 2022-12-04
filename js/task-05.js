28f1638720e25074a5c2646993ea0266d245b3a7
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-04 17:56:23 +0200

    add

--- js/task-05.js
+++ js/task-05.js
@@ -1,32 +1,45 @@
-
-const formInput = document.querySelector(`#name-input`);
-const formOutput= document.querySelector(`#name-output`);
-
-
-function changeNameIf(event) {
-   if (event.currentTarget.value !== "") {
-     formOutput.textContent = event.currentTarget.value;
-   } else {
-     formOutput.textContent = "Anonymous";
-    }
+const refs = {
+formInput: document.querySelector(`#name-input`),
+formOutput: document.querySelector(`#name-output`),
 }
-formInput.addEventListener('input', changeNameIf)
+
+refs.formInput.addEventListener(`input`, onInputChange);
+function onInputChange(event) {
+  if (event.currentTarget.value !== "") {
+     refs.formOutput.textContent = event.currentTarget.value;
+  } else {
+    refs.formOutput.textContent = `Anonymous`;
+  }
+}
 
 
 
 
-// const valueBtn = document.querySelector(`#value`);
-// const btnDecrement = document.querySelector(`[data-action="decrement"]`);
-// const btnIncrement = document.querySelector(`[data-action="increment"]`);
-// let counterValue = 0;
-// const increment = () => {
-//     counterValue += 1;
-//    valueBtn.textContent = counterValue;
-//     console.log(increment)
-// }
-// const decrement = () => {
-//     counterValue -= 1;
-//     valueBtn.textContent = counterValue;
-// };
-// btnIncrement.addEventListener('click', increment);
-// btnDecrement.addEventListener('click', decrement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
